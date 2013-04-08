b398a779c7059e29f06b0621455ab454fe3c6e05
0xD34D <clark@scheffsblend.com>
2013-04-08 12:31:41 -0400

    Add center clock (1/2)
    Patch Set 2:
      Add super_status_bar_center_clock layout for sw600dp devices
    
    Change-Id: Ibda358eac82e8f567d7e130c0f7d63b73d398cf5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2969,6 +2969,13 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
         /**
+         * Whether or not to use a center clock in phone status bar
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_CENTER_CLOCK = "status_bar_center_clock";
+
+        /**
          * whether which Ram Usage Bar mode is used on recent switcher
          * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
          * @hide

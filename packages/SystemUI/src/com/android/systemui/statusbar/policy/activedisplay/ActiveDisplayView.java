52ca2e005851beb1f2ed9d643ad2cf691a75c656
0xD34D <clark@scheffsblend.com>
2013-09-22 16:07:44 -0700

    Active display: make sure mNotificaiton is set correctly
    Change-Id: I4111ca843e340cca4d6dac4b2c2922ce3039e487

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -525,7 +525,7 @@ public class ActiveDisplayView extends FrameLayout {
             storedDraw.add(new TargetDrawable(res, getLayeredDrawable(activeBack,
                     mNotificationDrawable, targetInset, false)));
             storedDraw.add(new TargetDrawable(res, null));
-            if (mNotification.isClearable()) {
+            if (mNotification != null && mNotification.isClearable()) {
                 storedDraw.add(new TargetDrawable(res, res.getDrawable(R.drawable.ic_ad_dismiss_notification)));
             } else {
                 storedDraw.add(new TargetDrawable(res, null));
@@ -923,6 +923,7 @@ public class ActiveDisplayView extends FrameLayout {
             mNotificationDrawable = pkgContext.getResources().getDrawable(sbn.getNotification().icon);
             mCurrentNotificationIcon.setImageDrawable(mNotificationDrawable);
             setHandleText(sbn);
+            mNotification = sbn;
             mGlowPadView.post(new Runnable() {
                 @Override
                 public void run() {

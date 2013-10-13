2698f225a9a516ccb9fa1161a97091bce2e26eb0
Daniel MM <daxx4182@gmail.com>
2013-10-13 16:02:17 +0800

    Port forward: HALO tile (1/2)
    PS2 - fix the incomplete handler
    PS3 - final fixes
    
    Change-Id: I5d588959b4382fb9b9a11d25f9e7bb76a8e55d7a
    
    Conflicts:
    	packages/SystemUI/res/drawable-hdpi/ic_qs_halo_off.png
    	packages/SystemUI/res/drawable-hdpi/ic_qs_halo_on.png
    	packages/SystemUI/res/drawable-xhdpi/ic_qs_halo_off.png
    	packages/SystemUI/res/drawable-xhdpi/ic_qs_halo_on.png
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -46,6 +46,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.cm.QSConstants.TILE_WIMAX;
+import static com.android.internal.util.cm.QSConstants.TILE_HALO;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -98,6 +99,7 @@ import com.android.systemui.quicksettings.VolumeTile;
 import com.android.systemui.quicksettings.WiFiDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
+import com.android.systemui.quicksettings.HaloTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -272,6 +274,8 @@ public class QuickSettingsController {
                 if (QSUtils.adbEnabled(resolver)) {
                     qs = new NetworkAdbTile(mContext, this);
                 }
+            } else if (tile.equals(TILE_HALO)) {
+                qs = new HaloTile(mContext, this, mHandler);
             }
 
             if (qs != null) {
